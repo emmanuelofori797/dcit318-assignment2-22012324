7ad41c848da2af31955e324774afd5075bb3f9ac
emmanuelofori797 <eofori113@st.ug.edu.gh>
2026-09-02 21:04:55 +0000

    Complete abstract classes and methods

---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+abstract class Shape
+{
+    public abstract double GetArea();
+}
+
+class Circle : Shape
+{
+    private double radius;
+
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Rectangle : Shape
+{
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width)
+    {
+        this.length = length;
+        this.width = width;
+    }
+
+    public override double GetArea()
+    {
+        return length * width;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Circle circle = new Circle(5);
+        Rectangle rectangle = new Rectangle(10, 4);
+
+        Console.WriteLine("Area of Circle: " + circle.GetArea());
+        Console.WriteLine("Area of Rectangle: " + rectangle.GetArea());
+    }
+}
